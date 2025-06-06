dd225e05ef2234ce16316849d91b6e500c4af4d1
VacBanYT <111865770+VacBanYT@users.noreply.github.com>
2025-06-06 17:41:57 +0200

    fix: avoid shading PlaceholderAPI

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -107,8 +107,7 @@ dependencies {
 
     shadow("com.sk89q.worldguard:worldguard-bukkit:7.0.5")
     shadow("net.milkbowl.vault:VaultAPI:1.7")
-    shadow("me.clip:placeholderapi:2.11.6") {
-//        because("PlaceholderAPI on versions higher than 2.10.9 causes GH-1700 for some unknown reason")
+    compileOnly("me.clip:placeholderapi:2.11.6") {
         exclude(group = "com.google.code.gson", module = "gson")
     }
     shadow("com.gmail.filoghost.holographicdisplays:holographicdisplays-api:2.4.9")
